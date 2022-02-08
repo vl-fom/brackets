92f58f060956cff001608829ef6ecb2c071a5c82
vl-fom <mishyak.as@gmail.com>
2022-02-08 19:56:34 +0300

    bkjbkj

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  br = bracketsConfig.flat();
+  let stack = [];
+  for (i of str) {
+    if (stack.length === 0) {
+      stack.push(i);
+    } else if (br.indexOf(i) % 2 === 0 && br.indexOf(i) === br.lastIndexOf(i)) {
+      stack.push(i);
+    } else if (br.indexOf(i) % 2 === 0 && br.indexOf(i) + 1 === br.lastIndexOf(i) && stack.slice(-1).toString() !== i) {
+      stack.push(i);
+    } else if (br.lastIndexOf(i) % 2 === 1 && (br.indexOf(stack.slice(-1).toString()) + 1 ) !== br.lastIndexOf(i)) {
+      stack.push(i);
+    } else {
+      stack.pop();
+    }
+  }
+  return stack.length === 0 
 }
+
+
