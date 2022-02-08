0382063112ed3d1fe1b20d9bd95b1329c32c882b
vl-fom <mishyak.as@gmail.com>
2022-02-08 19:58:57 +0300

    bkjbkj

--- src/index.js
+++ src/index.js
@@ -8,13 +8,13 @@ module.exports = function check(str, bracketsConfig) {
       stack.push(i);
     } else if (br.indexOf(i) % 2 === 0 && br.indexOf(i) + 1 === br.lastIndexOf(i) && stack.slice(-1).toString() !== i) {
       stack.push(i);
-    } else if (br.lastIndexOf(i) % 2 === 1 && (br.indexOf(stack.slice(-1).toString()) + 1 ) !== br.lastIndexOf(i)) {
+    } else if (br.lastIndexOf(i) % 2 === 1 && br.indexOf(stack.slice(-1).toString()) + 1 !== br.lastIndexOf(i)) {
       stack.push(i);
     } else {
       stack.pop();
     }
   }
-  return stack.length === 0 
+  return stack.length === 0;
 }
 
 
