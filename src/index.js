e6a9cd2ae4cb1545fff4d6e623e79cad2e00d21d
Vladislav Fomenko <79394177+vl-fom@users.noreply.github.com>
2022-02-09 18:19:35 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,16 +2,10 @@ module.exports = function check(str, bracketsConfig) {
   br = bracketsConfig.flat();
   let stack = [];
   for (i of str) {
-    if (stack.length === 0) {
-      stack.push(i);
-    } else if (br.indexOf(i) % 2 === 0 && br.indexOf(i) === br.lastIndexOf(i)) {
-      stack.push(i);
-    } else if (br.indexOf(i) % 2 === 0 && br.indexOf(i) + 1 === br.lastIndexOf(i) && stack.slice(-1).toString() !== i) {
-      stack.push(i);
-    } else if (br.lastIndexOf(i) % 2 === 1 && br.indexOf(stack.slice(-1).toString()) + 1 !== br.lastIndexOf(i)) {
-      stack.push(i);
-    } else {
+    if (br.indexOf(stack.slice(-1).toString()) + 1 === br.lastIndexOf(i)) {
       stack.pop();
+    } else {
+      stack.push(i);
     }
   }
   return stack.length === 0;
